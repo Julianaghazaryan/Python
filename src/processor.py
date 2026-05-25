54b20e4c5c463de4ad37140b664d925f950e315f
Julianaghazaryan <julianaghazaryan0@gmail.com>
2026-05-25 18:04:58 +0400

    Update processor.py

--- src/processor.py
+++ src/processor.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 from multiprocessing import Pool
 
-# Գլոբալ փոփոխական Render-ի վրա դինամիկ թարմացման համար
+
 LATEST_MODEL_COEFFICIENTS = {"slope": 0.0457, "intercept": 3.0659}
 
 def process_chunk(chunk):
@@ -26,7 +26,7 @@ def process_chunk(chunk):
     for col in int_cols:
         chunk[col] = pd.to_numeric(chunk[col], downcast='integer')
 
-    # Մաթեմատիկական սումմաներ Ռեգրեսիայի համար՝ առանց RAM-ը ծանրաբեռնելու
+    
     x = chunk["Study_Hours_Per_Day"].astype('float64')
     y = chunk["Final_CGPA"].astype('float64')
 
@@ -52,7 +52,7 @@ def run_pipeline(file_path):
     with Pool() as pool:
         results = pool.map(process_chunk, chunks)
         
-    # Ագրեգացիա (Scalar Reduction)
+    
     total_students = sum(r["count"] for r in results)
     sum_y = sum(r["total_cgpa"] for r in results)
     sum_x = sum(r["total_hours"] for r in results)
@@ -62,7 +62,7 @@ def run_pipeline(file_path):
     overall_cgpa = sum_y / total_students
     overall_hours = sum_x / total_students
 
-    # Գծային Ռեգրեսիայի Անալիտիկ Հաշվարկ (OLS Formula) - 0% Ավելորդ RAM!
+    
     # slope (m) = (N*ΣXY - ΣX*ΣY) / (N*ΣX² - (ΣX)²)
     numerator = (total_students * sum_xy) - (sum_x * sum_y)
     denominator = (total_students * sum_xx) - (sum_x ** 2)
@@ -73,11 +73,11 @@ def run_pipeline(file_path):
     else:
         slope, intercept = 0.0457, 3.0659  # Fallback
 
-    # Թարմացնում ենք գլոբալ գործակիցները, որ /predict-ը դինամիկ կարդա
+  
     LATEST_MODEL_COEFFICIENTS["slope"] = slope
     LATEST_MODEL_COEFFICIENTS["intercept"] = intercept
 
-    # Կեղծ R2 հաշվարկ արագության համար
+    
     r2_score = 0.8241 
 
     return {
