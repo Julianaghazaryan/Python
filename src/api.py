c9ff15d93a5941b73f6ffda92628efb2dabd1b57
Julianaghazaryan <julianaghazaryan0@gmail.com>
2026-05-25 17:50:10 +0400

    Refactor prediction logic to use latest coefficients
    Updated prediction calculation to use dynamic model coefficients.

--- src/api.py
+++ src/api.py
@@ -1,27 +1,28 @@
 from fastapi import FastAPI, HTTPException
-from src.processor import run_pipeline
-from src.model import PredictionResponse  # ՆՈՐ՝ Կապում ենք model.py-ն
+from src.processor import run_pipeline, LATEST_MODEL_COEFFICIENTS
+from src.model import PredictionResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
+
 app = FastAPI(
     title="High-Performance Student Data Analytics Engine",
     description="FastAPI microservice utilizing multiprocessing and machine learning."
 )
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/ui")
 def get_ui():
     return FileResponse("static/index.html")
+
 @app.get("/")
 def read_root():
     return {"message": "Ուսանողների վերլուծության համակարգը միացված է"}
 
-
 @app.post("/process", response_model=PredictionResponse)
 def process_data():
     file_path = "data/Student_data.csv"
-    
     try:
         analysis_results = run_pipeline(file_path)
         return {
@@ -35,16 +36,15 @@ def process_data():
         )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 class PredictionInput(BaseModel):
     hours: float
 
 @app.post("/predict")
 def get_prediction(input_data: PredictionInput):
-   
-    slope = 0.0457
-    intercept = 3.0659
+    # ԴԻՆԱՄԻԿ ՍՏԱՑՈՒՄ. Կարդում է processor-ի հաշված վերջին գործակիցները
+    slope = LATEST_MODEL_COEFFICIENTS["slope"]
+    intercept = LATEST_MODEL_COEFFICIENTS["intercept"]
     
- 
     prediction = (input_data.hours * slope) + intercept
-    
     return {"prediction": round(float(prediction), 2)}
